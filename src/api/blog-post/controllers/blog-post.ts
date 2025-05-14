a0b370a74f865d13a1b0073f891d21586f11db5a
Moritz Roessler <morrossl@publicisgroupe.net>
2025-05-14 16:08:18 +0200

    fix: restore findOne route

--- src/api/blog-post/controllers/blog-post.ts
+++ src/api/blog-post/controllers/blog-post.ts
@@ -3,30 +3,6 @@ import { factories } from "@strapi/strapi";
 export default factories.createCoreController(
   "api::blog-post.blog-post",
   ({ strapi }: any) => ({
-    async findOne(ctx) {
-      // Fetch the blog post as usual
-      const { id } = ctx.params;
-      const query = ctx.query;
-
-      // Get the post
-      const entity = await strapi.entityService.findOne(
-        "api::blog-post.blog-post",
-        id,
-        query
-      );
-
-      if (entity) {
-        // Increment the view count
-        await strapi.entityService.update("api::blog-post.blog-post", id, {
-          data: {
-            views: (entity.views || 0) + 1,
-          },
-        });
-      }
-
-      // Return the post (optionally, you can re-fetch to get the updated views)
-      return this.transformResponse(entity);
-    },
     async incrementViews(ctx) {
       const { id } = ctx.params;
       // Fetch the current entity to check if views is null
