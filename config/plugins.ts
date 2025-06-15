db8d40220b1d3e0bf144640aff87b2dfd327962c
Moritz Roessler <moritz.roessler@digitas.com>
2025-06-15 19:09:46 +0200

    feat: switch to local assets provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,16 +2,24 @@ export default ({ env }) => ({
   i18n: {
     enabled: true,
   },
+  // upload: {
+  //   config: {
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       accessKeyId: env("AWS_ACCESS_KEY_ID"),
+  //       secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+  //       region: env("AWS_REGION"),
+  //       params: {
+  //         Bucket: env("AWS_BUCKET"),
+  //       },
+  //     },
+  //   },
+  // },
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: "local",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET"),
-        },
+        sizeLimit: 1000000, // optional: limit in bytes
       },
     },
   },
